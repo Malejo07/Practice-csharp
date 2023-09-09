435a37772e9fc65b1bef6740197d193af0f61d36
Mario Alejandro Jaramillo <malejo9002@gmail.com>
2023-09-09 15:20:24 -0500

    Finish program conditional_and_operators

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Conditionals*/
+// Console.WriteLine("Ingrese un número: ");
+// int num = int.Parse(Console.ReadLine());
+// if (num > 0) Console.WriteLine("Positivo");
+// else if (num < 0)Console.WriteLine("Negativo");
+// else Console.WriteLine("es cero");
+
+/*End conditionals*/
+
+/*Operators*/
+using System.Reflection.Metadata;
+
+Console.WriteLine("Ingrese el primer npumero: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo npumero: ");
+double b = double.Parse(Console.ReadLine());
+const string Indeterminado = "Indeterminado";
+Console.WriteLine($"Suma: ({a+b}, Resta: {a-b}, Multiplicación: {a*b}, DIvisión: {(b!=0 ? a/b: Indeterminado)}");
+
+
+
+
+
+
+
+/*End Operators*/
